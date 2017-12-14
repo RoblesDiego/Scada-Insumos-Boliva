ee3d46aa7afd1ad0509fc69bb70200e7a3526226
RoblesDiego <kael.r1991@gmail.com>
2017-12-14 11:30:37 -0400

    Soy yo

--- Scada Insumos Bolivia/Scada Insumos Bolivia/Form1.Designer.cs
+++ Scada Insumos Bolivia/Scada Insumos Bolivia/Form1.Designer.cs
@@ -34,11 +34,11 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(153, 156);
+            this.label1.Location = new System.Drawing.Point(97, 51);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.Size = new System.Drawing.Size(39, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Hola de nuevo";
+            this.label1.Text = "Soy yo";
             // 
             // Form1
             // 
